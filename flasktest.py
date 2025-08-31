5299d6607486e12a81e102fa642cd6febfbcad19
Spencer Neissen <73355319+hippy11111@users.noreply.github.com>
2025-08-31 01:43:13 -0400

    changed some stuff, hopefully works

--- flasktest.py
+++ flasktest.py
@@ -52,4 +52,5 @@ def main():
     return render_template("main.html", courselist=courselist, selected_course=selected_course, selected_quarter=selected_quarter, assignments=assignments)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
